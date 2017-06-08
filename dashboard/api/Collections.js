d24f2112609f59e508c509a91583f30de1830d6f
Kasper Moerch Zederkof <kaspermz@hotmail.com>
2017-06-08 16:01:01 +0200

    Added ES6 syntax changes to publication

--- dashboard/api/Collections.js
+++ dashboard/api/Collections.js
@@ -4,9 +4,7 @@ import { Meteor } from 'meteor/meteor';
 const CarsCollection = new Mongo.Collection('cars');
 
 if(Meteor.isServer) {
-  Meteor.publish('cars.all_cars', function() {
-    return CarsCollection.find();
-  });
+  Meteor.publish('cars.all_cars', () => CarsCollection.find());
 }
 
 export default CarsCollection;
