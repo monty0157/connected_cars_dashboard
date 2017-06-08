e9daa8e71bccbfccbcd5761954fa1e1646f95c67
Kasper Moerch Zederkof <kaspermz@hotmail.com>
2017-06-08 12:47:40 +0200

    Created main Cars component

--- dashboard/components/Cars.jsx
+++ dashboard/components/Cars.jsx
@@ -1,17 +1,37 @@
 import React from 'react';
-import { Layout, Card } from 'antd';
+import { Layout, Card, Menu, Icon } from 'antd';
 import Wrapper from './Wrapper';
 import CarsCollection from '../api/Collections'
 
-const { Content } = Layout;
+const { Content, Sider } = Layout;
+const { SubMenu } = Menu;
 
 const Cars = function Cars() {
-CarsCollection.insert({model: 'TELSA'})
+
+  const cars = CarsCollection.findOne();
+  console.log(cars)
   return(
     <Wrapper>
-      <Content>
-        <Card />
-      </Content>
+      <Layout>
+        <Layout style={{ padding: '24px 0', background: '#fff' , marginLeft: 85, marginRight: 85}}>
+          <Sider width={150} style={{ background: '#fff' }}>
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={['1']}
+              defaultOpenKeys={['sub1']}
+              className="h-100"
+            >
+              <Menu.Item key="1">option1</Menu.Item>
+              <Menu.Item key="2">option2</Menu.Item>
+              <Menu.Item key="3">option3</Menu.Item>
+              <Menu.Item key="4">option4</Menu.Item>
+            </Menu>
+          </Sider>
+          <Content style={{ padding: '0 24px', minHeight: 400 }}>
+            Content
+          </Content>
+        </Layout>
+      </Layout>
     </Wrapper>
   )
 }
