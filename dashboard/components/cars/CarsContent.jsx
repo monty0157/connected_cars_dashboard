721756e69fdc7d71f4538b8d78571120050ec6ff
Kasper Moerch Zederkof <kaspermz@hotmail.com>
2017-06-09 13:30:05 +0200

    Added option to change car name and age

--- dashboard/components/cars/CarsContent.jsx
+++ dashboard/components/cars/CarsContent.jsx
@@ -1,15 +1,77 @@
 import React from 'react';
 import { Mongo } from 'meteor/mongo'
 import { createContainer } from 'meteor/react-meteor-data';
+import { Form, Input, Col, Row, Button } from 'antd';
+import { compose, withHandlers, withProps } from 'recompose';
+
 import CarsCollection from '../../api/Collections'
 
-const CarsContent = function CarsContent({findCars, car, loading}) {
+const FormItem = Form.Item;
+
+const CarsContent = function CarsContent({car, form = {}, loading, handleSubmit}) {
   if(loading) return null;
+  const { getFieldDecorator, validateFields, resetFields } = form;
 
   return(
-    <h1>{car.model}</h1>
+    <div>
+      <h1>{car.model} - {car.age}</h1>
+      <Form onSubmit={(e) => handleSubmit({e, validateFields, resetFields})}>
+        <Row
+          gutter={12}
+          justify="space-between"
+        >
+          <Col span={8}>
+            <FormItem label="Bilens navn">
+                {getFieldDecorator("car_name")(
+
+                  <Input size="default" />
+                )}
+            </FormItem>
+          </Col>
+
+          <Col span={8}>
+            <FormItem label="Bilens navn">
+              {getFieldDecorator("car_age")(
+
+                <Input size="default" />
+              )}
+            </FormItem>
+          </Col>
+        </Row>
+        <Button
+          label="Submit"
+          htmlType="submit"
+          type="primary"
+        >
+          Submit
+        </Button>
+      </Form>
+    </div>
   )
 }
+
+const CarsContentForm = Form.create({})(
+  compose(
+    withProps(({id}) => ({
+      handleSubmit({e, validateFields, resetFields}) {
+        e.preventDefault();
+        validateFields((err, values) => {
+          if (!err) {
+            CarsCollection.update({"_id" : id}, {
+              $set: {
+                model: values.car_name,
+                age: values.car_age,
+              }
+            });
+            console.log('Received values of form: ', values);
+            resetFields();
+          }
+        });
+      }
+    }))
+  )(CarsContent)
+);
+
 
 const CarsContentContainer = createContainer(({params}) => {
   const { _id } = params;
@@ -19,7 +81,8 @@ const CarsContentContainer = createContainer(({params}) => {
   return {
     car: CarsCollection.findOne({_id: ObjectId}),
     loading: !subscribe.ready(),
+    id: ObjectId,
   }
-}, (CarsContent))
+}, (CarsContentForm))
 
 export default CarsContentContainer;
